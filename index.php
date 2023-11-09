d4df0d02c86b3e123cda44fe0ee12eb1938aa64c
wafizaki <wafizakihanif@gmail.com>
2023-11-10 01:29:58 +0700

    :feat: add tugas crud

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pendaftaran Siswa Baru | SMK Wafi</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <div class="wrapper">
+    <h3>Pendaftaran Siswa Baru<br>SMK Wafi</h3>
+            <ul>
+                <li><a href="form-daftar.php">Daftar Baru</a></li>
+                <li><a href="list-siswa.php">Pendaftar</a></li>
+            </ul>
+    </div>
+
+    </body>
+</html>
